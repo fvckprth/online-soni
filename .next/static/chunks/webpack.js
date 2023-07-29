d2b5d065837a74bdb26d9b9174816781fdc2dd67
fvckprth <parth@qortor.com>
2023-07-29 18:27:33 -0400

    Added arrows to MainContent

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a0d7e12122021da4"; }
+/******/ 		__webpack_require__.h = function() { return "9d060527bf8cd363"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
