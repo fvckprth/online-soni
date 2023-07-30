166faf016273586c070d0c953efdbd5994e6294d
fvckprth <parth@qortor.com>
2023-07-30 09:02:52 -0400

    Mute button specific changes

--- components/VideoDisplay.tsx
+++ components/VideoDisplay.tsx
@@ -14,9 +14,12 @@ function VideoDisplay({ isMinimized, onToggle }: VideoDisplayProps) {
   const containerStyles = isMinimized 
     ? 'top-2 right-2 w-28 h-48' 
     : 'top-0 left-0 w-full h-screen';
-  const toggleMute = () => {
-    setIsMuted(!isMuted);
-  };
+
+    const toggleMute = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.stopPropagation();  // Stop event propagation
+        setIsMuted(!isMuted);
+    };
+    
 
   return (
     <div className={`fixed ${containerStyles}`} onClick={onToggle}>
@@ -26,7 +29,7 @@ function VideoDisplay({ isMinimized, onToggle }: VideoDisplayProps) {
             preferPlayback
             playbackId="DsWrR026AJqjOtlKB02HuCROgAPzwCP2ok0002HxsJy3pcY"
             preLoad="auto"
-            autoPlay="muted"
+            autoPlay="any"
             loop
             playInline
             maxResolution="1080p"
