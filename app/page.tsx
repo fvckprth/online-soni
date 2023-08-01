3f73f23c2bb2266c767e7b9514f29943069fa181
fvckprth <parth@qortor.com>
2023-08-01 12:36:12 -0400

    added plausible to page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import Header from '@/components/Header';
+import PlausibleProvider from 'next-plausible';
 import MainContent from '@/components/MainContent';
 import Footer from '@/components/Footer';
 import VideoDisplay from '@/components/VideoDisplay';
@@ -16,24 +17,26 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col justify-between md:justify-center h-full items-center mx-4 md:px-0">
-      {!initialLoad && (
-        <>
-          <div className={`w-full md:w-[860px] m-4 md:m-6 ${!isMinimized ? 'inverted-text on-top' : 'text-black'}`}>
-            <Header />
-          </div>
+    <PlausibleProvider domain="onlinesoni.com">
+      <div className="flex flex-col justify-between md:justify-center h-full items-center mx-4 md:px-0">
+        {!initialLoad && (
+          <>
+            <div className={`w-full md:w-[860px] m-4 md:m-6 ${!isMinimized ? 'inverted-text on-top' : 'text-black'}`}>
+              <Header />
+            </div>
 
-          <div className="w-full md:w-[860px]">
-            <MainContent />
-          </div>
+            <div className="w-full md:w-[860px]">
+              <MainContent />
+            </div>
 
-          <div className={`w-full md:w-[860px] m-4 md:m-6 ${!isMinimized ? 'inverted-text on-top' : 'text-black'}`}>
-            <Footer />
-          </div>
-        </>
-      )}
+            <div className={`w-full md:w-[860px] m-4 md:m-6 ${!isMinimized ? 'inverted-text on-top' : 'text-black'}`}>
+              <Footer />
+            </div>
+          </>
+        )}
 
-      <VideoDisplay isMinimized={isMinimized} onToggle={handleToggle} />
-    </div>
+        <VideoDisplay isMinimized={isMinimized} onToggle={handleToggle} />
+      </div>
+    </PlausibleProvider>
   );
 }
